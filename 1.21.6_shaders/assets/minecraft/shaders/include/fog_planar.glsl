dfa124101b5db4f0361d4e9d17f35c2d64cbd1fa
MrGlitchDogePE <hexygaming99@gmail.com>
2025-05-23 11:31:08 -0600

    udpated

--- 1.21.6_shaders/assets/minecraft/shaders/include/fog_planar.glsl
+++ 1.21.6_shaders/assets/minecraft/shaders/include/fog_planar.glsl
@@ -14,7 +14,7 @@ layout(std140) uniform Fog {
 
 float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
     float adjustedFogStart = fogStart / 3.5969351459790425888109963041923;
-    float adjustedFogEnd = fogEnd * 0.99919548688714581058448359296267;
+    float adjustedFogEnd = fogEnd * 1.0104116005108058529096670816872;
 
     if (vertexDistance <= adjustedFogStart) {
         return 0.0;
