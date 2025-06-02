6070c249855f1058cedd0ae361a85c7aab358640
MrGlitchDogePE <hexygaming99@gmail.com>
2025-06-02 12:58:10 -0600

    added sodium support

---
+++ 1.21.6_shaders/assets/minecraft/shaders/include/fog_cylinder.glsl
@@ -0,0 +1,43 @@
+#version 150
+
+layout(std140) uniform Fog {
+    vec4 FogColor;
+    float FogEnvironmentalStart;
+    float FogEnvironmentalEnd;
+    float FogRenderDistanceStart;
+    float FogRenderDistanceEnd;
+    float FogSkyEnd;
+    float FogCloudsEnd;
+};
+
+float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
+    float adjustedFogStart = fogStart / 3.5969351459790425888109963041923;
+    float adjustedFogEnd = fogEnd * 1.0132739486807940002673196089301;
+
+    if (vertexDistance <= adjustedFogStart) {
+        return 0.0;
+    } else if (vertexDistance >= adjustedFogEnd) {
+        return 1.0;
+    }
+
+    return (vertexDistance - adjustedFogStart) / (adjustedFogEnd - adjustedFogStart);
+}
+
+float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
+    return max(linear_fog_value(sphericalVertexDistance, environmentalStart, environmentalEnd), linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd));
+}
+
+vec4 apply_fog(vec4 inColor, float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd, vec4 fogColor) {
+    float fogValue = total_fog_value(sphericalVertexDistance, cylindricalVertexDistance, environmentalStart, environmentalEnd, renderDistanceStart, renderDistanceEnd);
+    return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
+}
+
+float fog_spherical_distance(vec3 pos) {
+    return length(pos);
+}
+
+float fog_cylindrical_distance(vec3 pos) {
+    float distXZ = length(pos.xz);
+    float distY = abs(pos.y);
+    return max(distXZ, distY);
+}
