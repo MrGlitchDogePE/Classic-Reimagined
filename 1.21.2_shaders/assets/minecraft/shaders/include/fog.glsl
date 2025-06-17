72202725db29e3daafc16c20d71b08d4503e8029
MrGlitchDogePE <hexygaming99@gmail.com>
2025-06-16 19:02:41 -0600

    updated assets

--- 1.21.2_shaders/assets/minecraft/shaders/include/fog.glsl
+++ 1.21.2_shaders/assets/minecraft/shaders/include/fog.glsl
@@ -3,33 +3,26 @@
 vec4 linear_fog(vec4 inColor, float vertexDistance, float fogStart, float fogEnd, vec4 fogColor) {
     // Apply adjustments to fogStart and fogEnd
     fogStart = fogStart / 3.6959913571644626334854628038583;
-    fogEnd = fogEnd * 1.06;
+    fogEnd = fogEnd * 1.03;
 
     if (vertexDistance <= fogStart) {
         return inColor;
     }
 
-    float fogValue = vertexDistance < fogEnd ? smoothstep(fogStart, fogEnd, vertexDistance) : 1.0;
+    float fogValue = vertexDistance < fogEnd ? (vertexDistance - fogStart) / (fogEnd - fogStart) : 1.0;
     return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
 }
 
 float linear_fog_fade(float vertexDistance, float fogStart, float fogEnd) {
     if (vertexDistance <= fogStart) {
-        return 0.0;
-    } else if (vertexDistance >= fogEnd) {
         return 1.0;
-        } return (vertexDistance - fogStart) / (fogEnd - fogStart);
+    } else if (vertexDistance >= fogEnd) {
+        return 0.0;
+    }
+
+    return smoothstep(fogEnd, fogStart, vertexDistance);
 }
-float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    return max(linear_fog_fade(sphericalVertexDistance, environmentalStart, environmantalEnd), linear_fog_fade(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd));
-}
-vec4 apply_fog(vec4 inColor, float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd, vec4 fogColor) {
-    float fogValue = total_fog_value(sphericalVertexDistance, cylindricalVertexDistance, environmentalStart, environmantalEnd, renderDistanceStart, renderDistanceEnd);
-    return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
-}
-float fog_spherical_distance(vec3 pos) {
-    return length(pos);
-}
+
 float fog_distance(vec3 pos, int shape) {
         return length(pos);
 }
