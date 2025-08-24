b4db1c9e6159ac642a0e8cf39eaf807b1a33e1cd
Hexy <hexygaming99@gmail.com>
2025-08-23 20:20:23 -0600

    Update betalight.glsl

--- 1.21.9_shaders/assets/minecraft/shaders/include/betalight.glsl
+++ 1.21.9_shaders/assets/minecraft/shaders/include/betalight.glsl
@@ -1,23 +1,8 @@
 #ifndef _BETA_LIGHT_GLSL
 #define _BETA_LIGHT_GLSL
 
-const float BETA_LIGHT[16] = float[](
-    12,
-    16,
-    21,
-    26,
-    32,
-    39,
-    47,
-    56,
-    66,
-    78,
-    93,
-    111,
-    133,
-    162,
-    201,
-    255
+const int BETA_LIGHT[16] = int[](
+    12, 16, 21, 26, 32, 39, 47, 56, 66, 78, 93, 111, 133, 162, 201, 255
 );
 
 #endif // _BETA_LIGHT_GLSL
